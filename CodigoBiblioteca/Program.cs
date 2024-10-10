b80b9299481b69c8a0cb851cd135979f5019b678
joão <joaopaulo102003@gmail.com>
2024-10-10 10:16:33 -0300

    Testes com Lista e Classes

---
+++ CodigoBiblioteca/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public class Login
+{
+    public string usuario {get; set;}
+    public string senha {get; set;}
+}
+    
+
+    static void Main()
+    {
+        List<Login> ListaLogin = new List<Login>();
+        bool pls = false;
+    do
+    {
+        Login posicao = new Login();
+        posicao.usuario = Console.ReadLine();
+        posicao.senha = Console.ReadLine();
+        ListaLogin.Add(posicao);
+        if (posicao.usuario=="sair")
+        {
+            pls = true;
+        }
+    }while(pls == false);
+        foreach(Login posicao in ListaLogin){
+       Console.WriteLine(posicao.usuario);
+        }       
+    }
+}

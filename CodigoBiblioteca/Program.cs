27e17eed3eefce5907163fc25116efb9ec1aaf88
João <joaopaulo102003@gmail.com>
2024-10-16 08:32:27 -0300

    função Login pronta

--- CodigoBiblioteca/Program.cs
+++ CodigoBiblioteca/Program.cs
@@ -1,29 +1,37 @@
-﻿class Program
+﻿using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Collections.Generic;
+class Program
 {
-    public class Login
-{
-    public string usuario {get; set;}
-    public string senha {get; set;}
-}
-    
-
-    static void Main()
-    {
-        List<Login> ListaLogin = new List<Login>();
-        bool pls = false;
-    do
+    static List<Login> ListaLogin = new List<Login>();
+      static void login(string x, string y)
     {
         Login posicao = new Login();
-        posicao.usuario = Console.ReadLine();
-        posicao.senha = Console.ReadLine();
+        posicao.usuario = x;
+        posicao.senha = y;
         ListaLogin.Add(posicao);
-        if (posicao.usuario=="sair")
-        {
-            pls = true;
-        }
-    }while(pls == false);
-        foreach(Login posicao in ListaLogin){
-       Console.WriteLine(posicao.usuario);
-        }       
+        
     }
+    public class Login
+    {
+        public string usuario { get; set; }
+        public string senha { get; set; }
+                public override string ToString()
+        {
+            return $"Usuário: {usuario}, Senha: {senha}\n";
+        }
+    }
+    static void Main()
+    {
+        for(int i =0; i<4;i++)
+        {
+        login(Console.ReadLine(), Console.ReadLine());
+        }
+        for(int i =0; i<4;i++)
+        {
+        Console.WriteLine(ListaLogin[i]);
+        }
+
+    }
+  
 }
